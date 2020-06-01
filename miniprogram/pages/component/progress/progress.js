ac78c7707877e6e7e589689edd51db3490a8f677
￠幻冰 <z@uice.xyz>
2020-06-01 14:54:47 +0800

    系统进度条

--- miniprogram/pages/component/progress/progress.js
+++ miniprogram/pages/component/progress/progress.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    percentValue: 50
   },
 
   /**
@@ -62,5 +62,10 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  onTapReloadBtn: function() {
+    this.setData({ percentValue: 0 })
+    this.setData({ percentValue: 50 })
   }
 })
