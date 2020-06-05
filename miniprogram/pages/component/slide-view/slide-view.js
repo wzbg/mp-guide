691fd8beedfd64984d996f9efd0f8a28f9425305
￠幻冰 <z@uice.xyz>
2020-06-05 14:14:16 +0800

    小程序视图 Slide View

---
+++ miniprogram/pages/component/slide-view/slide-view.js
@@ -0,0 +1,82 @@
+// miniprogram/pages/component/slide-view/slide-view.js
+const base64 = require('../../../images/base64')
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      icon: base64.icon20,
+      slideButtons: [{
+        text: '普通1',
+        src: '/images/icon_love.svg'
+      }, {
+        text: '普通2',
+        extClass: 'test',
+        src: '/images/icon_star.svg'
+      }, {
+        type: 'warn',
+        text: '警示3',
+        extClass: 'test',
+        src: '/images/icon_del.svg'
+      }]
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
